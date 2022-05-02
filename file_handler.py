9c2da112330292bab5920cac797cc77933b95d36
MikhailKochetkov <consultant.dax@gmail.com>
2022-05-02 17:53:51 +0300

    refactoring

--- file_handler.py
+++ file_handler.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 
 class FileHandler:
     def open_image_name(self):
-        imagename = filedialog.askopenfilename(title='open')
+        imagename = filedialog.askopenfilename(title='open image')
         return imagename
 
     def open_model_name(self):
